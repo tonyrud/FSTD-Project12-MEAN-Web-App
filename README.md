c9c697da85044149a0d38688fb0e4c74438628dd
tonyrud <tonrudny@gmail.com>
2017-06-03 11:54:41 -0400

    updated readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# MEAN Hiking App
+
+## Development server
+Install dependencies
+```
+npm install
+```
+
+Run `npm start` for api and client side servers. The both servers will automatically reload if you change any of the source files.
+
+<a href="http://localhost:4000/api" target="_blank">API Server</a>
+
+<a href="http://localhost:4200/" target="_blank">Client Dev Server</a>
+
+Seed MongoDB for Dev
+```
+npm run seed
+```
--- app/handlers/errorHandlers.js
+++ app/handlers/errorHandlers.js
@@ -1,26 +1,23 @@
 /*
-  Catch Errors Handler
-
-  With async/await, needs this catch errors
+  async/await, needs this catch errors
   Instead of using try{} catch(e) {} in each controller, we wrap the function in
   catchErrors(), catch any errors they throw, and pass it along to our express middleware with next()
 */
 
 exports.catchAsyncErrors = (fn) => {
   return function (req, res, next) {
-    return fn(req, res, next).catch(next);
+    return fn(req, res, next).catch(next)
   };
-};
+}
 
 /*
   Not Found Error Handler
 */
 exports.notFound = (req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
 };
-
 
 /*
   Development Error Hanlder
@@ -28,22 +25,21 @@ exports.notFound = (req, res, next) => {
   In development, show error messages so if we hit a syntax error or any other previously un-handled error, we can show good info on what happened
 */
 exports.developmentErrors = (err, req, res, next) => {
-  err.stack = err.stack || '';
+  err.stack = err.stack || ''
   const errorDetails = {
     message: err.message,
     status: err.status,
     stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>')
-  };
-  res.status(err.status || 500);
+  }
+  res.status(err.status || 500)
   res.format({
     // Based on the `Accept` http header
     'text/html': () => {
-      res.render('error', errorDetails);
+      res.render('error', errorDetails)
     }, // Form Submit, Reload the page
     'application/json': () => res.json(errorDetails) // Ajax call, send JSON back
-  });
+  })
 };
-
 
 /*
   Production Error Handler
@@ -51,9 +47,9 @@ exports.developmentErrors = (err, req, res, next) => {
   Keep stack traces from user
 */
 exports.productionErrors = (err, req, res, next) => {
-  res.status(err.status || 500);
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
     error: {}
-  });
+  })
 };
