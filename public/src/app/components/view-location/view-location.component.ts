942a1260f909e95d842548041881b51a5ceb9716
tonyrud <tonrudny@gmail.com>
2017-07-05 21:32:28 -0400

    added location activites from a hiking api search

--- public/src/app/components/view-location/view-location.component.ts
+++ public/src/app/components/view-location/view-location.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '../../_interfaces/location.interface';
+import { TrailsService } from '../../_services/trails.service';
 
 @Component({
   selector: 'view-component',
@@ -8,18 +9,34 @@ import { Location } from '../../_interfaces/location.interface';
   styleUrls: ['./view-location.component.scss']
 })
 export class ViewLocationComponent implements OnInit {
-  viewData: any
+  location: any
+  activities: any
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private _trailsService: TrailsService
   ) { }
 
   ngOnInit() {
+
     this.route.queryParams
-      .subscribe((params: Params) => {
-        this.viewData = params
-        console.log(this.viewData)
+      .subscribe((params: any) => {
+        this._trailsService.searchTrails(`?lat=${params.lat}&limit=5&lon=${params.lon}&q[activities_activity_type_name_eq]=hiking&radius=1`).subscribe(location => {
+          this.location = location.places[0]
+          console.log(this.location)
+        })
       })
+  }
+
+  parseLocationParams (locationParams: any) {
+    const parsed = locationParams.activitiesNames.map((item, index: number) => {
+      return {
+        name: item,
+        url: locationParams.activitiesUrls[index],
+        description: locationParams.activitiesDescriptions[index],
+      }
+    })
+    return parsed
   }
 
 }
