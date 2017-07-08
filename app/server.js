bfa05a834a2a8012e971cc5c34942c8701af5512
tonyrud <tonrudny@gmail.com>
2017-07-08 18:37:21 -0400

    remove all public express paths

--- app/server.js
+++ app/server.js
@@ -13,12 +13,6 @@ require('./handlers/passport')
 // create Express app
 const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'pug')
-
-// serves up static files from the public folder. Anything in public/ will just be served up as the file it is
-app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, './../dist')))
 
 // Allows CORS
