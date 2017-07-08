1189b2c254a2abb35b60defac367c4252ed819c8
tonyrud <tonrudny@gmail.com>
2017-07-08 18:30:49 -0400

    build test

--- app/server.js
+++ app/server.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.static(path.join(__dirname, './../dist')))
 
 // Allows CORS
-app.use(cors())
+app.use(cors()) 
 // app.use((req, res, next) => {
 //   res.header('Access-Control-Allow-Origin', '*')
 //   res.header('Access-Control-Allow-Headers', 'Origin, X-requested-With, Content-Type, Accept, Authorization')
