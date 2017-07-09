fa34e0e1bd1d26677af649023830c509d7d4d5bd
tonyrud <tonrudny@gmail.com>
2017-07-09 10:27:08 -0400

    change prod to https

--- public/src/environments/environment.prod.ts
+++ public/src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ import { Environment } from '../app/_interfaces/environment.interface';
 
 export const environment: Environment = {
   production: true,
-  apiUrl: 'http://hiking-app.herokuapp.com/api/',
+  apiUrl: 'https://hiking-app.herokuapp.com/api/',
   app_config: APP_CONFIG
 }
